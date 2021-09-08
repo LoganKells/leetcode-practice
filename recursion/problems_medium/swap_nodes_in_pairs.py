efb705a3617a5fe671b28f3d96bea53a471a1456
LoganKells <logan.kells@pinnacletech.com>
2021-09-08 16:26:06 -0500

    Added iterative method

--- recursion/problems_medium/swap_nodes_in_pairs.py
+++ recursion/problems_medium/swap_nodes_in_pairs.py
@@ -6,6 +6,29 @@ from typing import List, Optional
 # 24. Swap Nodes in Pairs
 # see - https://leetcode.com/problems/swap-nodes-in-pairs/
 
+class SolutionIterative:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Create dummy head to start the cursor
+        dummy_head = ListNode()
+        dummy_head.next = head
+        cursor = dummy_head
+
+        while cursor is not None and cursor.next is not None and cursor.next.next is not None:
+            # Values to swap
+            first = cursor.next
+            second = cursor.next.next
+
+            # Swap first and second
+            cursor.next = second
+            first.next = second.next
+            second.next = first
+
+            # Select new cursor, with stride = 2
+            cursor = first  # stride = 2 (due to first being swapped to second position at this point)
+
+        # Return the final head after all swapping
+        return dummy_head.next
+
 
 class SolutionRecursive:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
@@ -26,6 +49,15 @@ class SolutionRecursive:
 
 
 test_cases = [([1, 2, 3, 4], [2, 1, 4, 3])]
+
+
+@pytest.mark.parametrize("nums, expected", test_cases)
+def test_swap_nodes_in_pairs_iterative(nums, expected):
+    sol = SolutionIterative()
+    head = build_linked_list(nums=nums)
+    head_swapped = sol.swapPairs(head=head)
+    head_swapped_nums = list_from_linked_list(head=head_swapped)
+    assert head_swapped_nums == expected
 
 
 @pytest.mark.parametrize("nums, expected", test_cases)
