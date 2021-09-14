b2cce7052f92de73005c2417c042a0b12ff10b0e
LoganKells <lkells@outlook.com>
2021-09-14 17:59:25 -0500

    code updated.

--- trees/unique_binary_search_trees_ii.py
+++ trees/unique_binary_search_trees_ii.py
@@ -1,41 +1,41 @@
 from typing import List, Optional
+from util.binary_tree import TreeNode
 
+# 95. Unique Binary Search Trees II
 # See - https://leetcode.com/problems/unique-binary-search-trees-ii/
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
 
 
 class Solution:
+    trees_all = []
+
     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
-        # generate a queue of tree node values, any of which can be root of a tree
+        # Recursively build the left and right sub trees, given some root value
+        trees = self.post_order_build(start=1, end=n)
+        return trees
 
-        def build_tree_recursive(start: int, end: int, tree_values: List[int]):
-            if start > end:
-                return [None, ]
+    def post_order_build(self, start: int, end: int):
+        # Base case
+        if start > end:
+            return [None, ]
 
-            trees = []
+        sub_tree = []
 
-            for i in range(start, end + 1):
-                # i: new root
-                left_values = list(range(start, i))
-                left_trees = build_tree_recursive(start=start, end=i - 1, tree_values=left_values)
-                right_values = list(range(i + 1, end))
-                right_trees = build_tree_recursive(start=i + 1, end=end, tree_values=right_values)
+        # Build subtrees based on new start value, i
+        for i in range(start, end + 1):
+            # Post-order traversal
+            # left_tree_values = list(range(start, i))
+            left_tree_values = self.post_order_build(start=start, end=i - 1)
 
-                # Create trees with given root, and remaining values
-                for left_value in left_trees:
-                    for right_value in right_trees:
-                        root = TreeNode(i)  # current sub-tree root
-                        root.left = left_value
-                        root.right = right_value
-                        trees.append(root)
-            return trees
+            # right_tree_values = list(range(i + 1, end))
+            right_tree_values = self.post_order_build(start=i + 1, end=end)
 
-        return build_tree_recursive(start=1, end=n, tree_values=list(range(1, n + 1)))
+            z= 1
+            # Add the values to the tree
+            for value_l in left_tree_values:
+                for value_r in right_tree_values:
+                    root = TreeNode(val=i, left=value_l, right=value_r)
+                    sub_tree.append(root)
+        return sub_tree
 
 
 def test_generate_tree():
