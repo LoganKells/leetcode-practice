5d0b5c4c9d2faec1363a602b6ba4202bce372bf4
LoganKells <lkells@outlook.com>
2023-09-12 06:59:19 -0400

    Organized tests as module

--- leetcode-javascript/src/arrays/2619_array_prototype_last.js
+++ leetcode-javascript/src/arrays/2619_array_prototype_last.js
@@ -31,6 +31,7 @@ const myArrayType = {
     }
 }
 
+
 export {myArrayType}
 
 
--- leetcode-javascript/src/arrays/__tests__/2619_array_prototype_last.test.js
+++ leetcode-javascript/src/arrays/__tests__/2619_array_prototype_last.test.js
@@ -1,19 +1,20 @@
-import {myArrayType} from "../2619_array_prototype_last";
+import {myArrayType} from "../2619_array_prototype_last.js";
 
-// const data = Object.create(myArrayType)
-Object.assign(Array.prototype, myArrayType);
+describe('test Array.prototype.last method', () => {
+    // Assign the prototype to add the .last method to Array objects.
+    Object.assign(Array.prototype, myArrayType);
 
+    test('returns value of last array element as 3', () => {
+            const arr = [1, 2, 3];
+            arr.last(); // 3
+            expect(arr.last()).toBe(3);
+        }
+    )
 
-test('returns value of last array element as 3', () => {
-    const arr = [1, 2, 3];
-    arr.last(); // 3
-    expect(arr.last()).toBe(3);
-    }
-)
-
-test('returns value of last array element as -1', () => {
-        const arr = [];
-        arr.last(); // -1
-        expect(arr.last()).toBe(-1);
-    }
-)
+    test('returns value of last array element as -1', () => {
+            const arr = [];
+            arr.last(); // -1
+            expect(arr.last()).toBe(-1);
+        }
+    )
+})
